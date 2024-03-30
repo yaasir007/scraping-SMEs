6bf9a521a7fc4d60900c185fee8834edd7302124
yaasir cheekoory <yaasir1997@gmail.com>
2024-03-30 23:40:22 +0400

    still bug in loop

--- sme.rb
+++ sme.rb
@@ -5,6 +5,7 @@ require 'csv'
 
 data_store_path = 'data.csv'
 url = 'https://smesdb.govmu.org/smesdb/index.php/directory-smes_e_direc'
+smes_links_array = []
 
 sme_file = URI.open(url).read
 sme_main_page = Nokogiri::HTML.parse(sme_file)
@@ -12,13 +13,16 @@ sme_main_page = Nokogiri::HTML.parse(sme_file)
 puts 'Starting to scrape the SME Directory'
 
 sme_main_page.search('.entry-content .elementor .elementor-section .elementor-container .elementor-element-populated .elementor-widget-shortcode .elementor-widget-container .elementor-shortcode .drts-view-shortcode .drts-view-entities-container .drts-view-post-entities  .drts-view-entities-list-grid .drts-row .drts-view-entity-container .drts-entity-post .drts-display-element a').each do |element|
-  index = 0
   sme_link = element.attribute('href').value
+  smes_links_array << sme_link
+end
 
-  single_sme_file = URI.open(sme_link).read
+smes_links_array.each do |link|
+  single_sme_file = URI.open(link).read
   single_sme_page = Nokogiri::HTML.parse(single_sme_file)
 
   single_sme_page.search('.drts-row').each do |sme_data|
+    index = 0
     puts 'Starting with the scraping'
 
     company_name = sme_data.search('.drts-display-element-entity_field_field_company_name-1 .drts-entity-field-value').text
