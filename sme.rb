db33629a6645d51a7991173fc9bc35bc11bbd4eb
yaasir cheekoory <yaasir1997@gmail.com>
2024-03-31 19:39:34 +0400

    wip

--- sme.rb
+++ sme.rb
@@ -6,6 +6,7 @@ require 'csv'
 data_store_path = 'data.csv'
 url = 'https://smesdb.govmu.org/smesdb/index.php/directory-smes_e_direc'
 smes_links_array = []
+all_data = []
 
 sme_file = URI.open(url).read
 sme_main_page = Nokogiri::HTML.parse(sme_file)
@@ -15,6 +16,10 @@ puts 'Starting to scrape the SME Directory'
 sme_main_page.search('.entry-content .elementor .elementor-section .elementor-container .elementor-element-populated .elementor-widget-shortcode .elementor-widget-container .elementor-shortcode .drts-view-shortcode .drts-view-entities-container .drts-view-post-entities  .drts-view-entities-list-grid .drts-row .drts-view-entity-container .drts-entity-post .drts-display-element a').each do |element|
   sme_link = element.attribute('href').value
   smes_links_array << sme_link
+end
+
+CSV.open(data_store_path, 'wb') do |csv|
+  csv << ['Number', 'Company Name', 'Other Name', 'Tel No', 'Mobile No', 'Email Address', 'Business Address 1', 'Product Details', 'Categories', 'District']
 end
 
 smes_links_array.each do |link|
@@ -35,11 +40,13 @@ smes_links_array.each do |link|
     business_category = sme_data.search('.drts-display-element-entity_field_field_categories-1 .drts-entity-field-value').text
     district_name = sme_data.search('.drts-display-element-entity_field_field_district-1 .drts-entity-field-value').text
 
-    CSV.open(data_store_path, 'wb') do |csv|
-      csv << ['Number', 'Company Name', 'Other Name', 'Tel No', 'Mobile No', 'Email Address', 'Business Address 1', 'Product Details', 'Categories', 'District']
-      csv << [index + 1, company_name, other_name, tel_number, mobile_number, email_add, business_add, product_details, business_category, district_name]
-    end
-
+    all_data << [index += 1, company_name, other_name, tel_number, mobile_number, email_add, business_add, product_details, business_category, district_name]
     puts "Finished with number #{index}"
   end
+end
+
+puts all_data
+
+CSV.open(data_store_path, 'wb') do |csv|
+  csv << all_data
 end
